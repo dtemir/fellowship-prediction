bddee7e3f48b84106f56c1e8196f9049c73e0000
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-22 14:34:50 +0300

    client : Navbar style updated

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: '#F9FAFB',
+      secondary: '#111827',
+      accent: '#E2A120',
+      paragraph:
+      {
+        100:'#374151',
+        200:'#1F2937'
+      }
+    },
     extend: {},
   },
   variants: {
