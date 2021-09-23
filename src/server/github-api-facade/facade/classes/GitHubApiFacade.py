6bd282a0df5c126ab0d1d61b4148ef0da2366b4e
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-23 17:53:10 +0300

    github-api-facade: get_feature, fetch_all_features methods implemented inside GitHubApiFacade class| methods & propeties visibility changed inside GitHubApiFacade class

--- src/server/github-api-facade/facade/classes/GitHubApiFacade.py
+++ src/server/github-api-facade/facade/classes/GitHubApiFacade.py
@@ -8,9 +8,8 @@ load_dotenv()
 class GitHubApiFacade:
     
     # Constructor
-    def __init__(self,username,features):
+    def __init__(self,username):
         self.__username   =   username
-        self.__features   =   features
         self.__token      =   os.getenv('GITHUB_TOKEN')
         self.__headers    =   {
                                 "Content-Type": "application/json",
@@ -19,7 +18,7 @@ class GitHubApiFacade:
                             }
         self.__base_url   = 'https://api.github.com'
 
-    # Methods 
+    # Private Methods 
 
     # generates an API_ENDPOINT , sends a request to it and return the results as json|None
     def __fetch_feature(self,feature):
@@ -44,16 +43,14 @@ class GitHubApiFacade:
         except: return None
 
     
-
-
     # returns the number of fellows that a user has on github
-    def get_followers(self): return self.__fetch_feature(API_FEATURES.FOLLOWERS.value)
+    def __get_followers(self): return self.__fetch_feature(API_FEATURES.FOLLOWERS.value)
         
     # returns the number of public repos that the user has on his github
-    def get_public_repos(self): return self.__fetch_feature(API_FEATURES.PUBLIC_REPOS.value)
+    def __get_public_repos(self): return self.__fetch_feature(API_FEATURES.PUBLIC_REPOS.value)
 
     # returns the total number of stars that a user has on his github
-    def get_total_stars(self):
+    def __get_total_stars(self):
         repos_data = self.__fetch_feature_route(API_FEATURES.STARGAZERS_COUNT.value)
         total_stars = 0 
 
@@ -63,7 +60,7 @@ class GitHubApiFacade:
         return total_stars
 
     # returns the total number of forks that other people made from the user github
-    def get_total_forks(self):
+    def __get_total_forks(self):
         repos_data = self.__fetch_feature_route(API_FEATURES.FORKS_COUNT.value)
         total_forks = 0
 
@@ -73,22 +70,40 @@ class GitHubApiFacade:
         return total_forks
 
     # returns the total number of commits that the user has on his github
-    def get_total_commits(self):
+    def __get_total_commits(self):
         commits_data = self.__fetch_feature_route(API_FEATURES.COMMITS.value)
         return commits_data['total_count']
 
     # returns the total number of issues 
-    def get_total_issues(self):
+    def __get_total_issues(self):
         issues_data = self.__fetch_feature_route(API_FEATURES.ISSUES.value)
         return issues_data['total_count']
 
     # returns the total number of organizations that the user is included in
 
-    def get_total_orgs(self):
+    def __get_total_orgs(self):
         orgs_data = self.__fetch_feature_route(API_FEATURES.ORGS.value)
         return  len(orgs_data)
 
 
+    # Public Methods
 
-       
+    # fetches the feature passed as an argument
+    def get_feature(self,feature):
+
+        if(feature == API_FEATURES.FOLLOWERS.value)         :   return self.__get_followers()
+        if(feature == API_FEATURES.PUBLIC_REPOS.value)      :   return self.__get_public_repos()
+        if(feature == API_FEATURES.STARGAZERS_COUNT.value)  :   return self.__get_total_stars()
+        if(feature == API_FEATURES.FORKS_COUNT.value)             :   return self.__get_total_forks()
+        if(feature == API_FEATURES.COMMITS.value)                 :   return self.__get_total_commits()
+        if(feature == API_FEATURES.ISSUES.value)                  :   return self.__get_total_issues()
+        if(feature == API_FEATURES.ORGS.value)                    :   return self.__get_total_orgs()
+
+
+    # returns a dictionary {feature:value}
+    def fetch_all_feature(self):
+        fetch_result = dict()
+        for feature in API_FEATURES:  fetch_result[feature.value] = self.get_feature(feature.value)
+        return fetch_result
+            
 
