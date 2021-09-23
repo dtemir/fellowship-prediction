81b77a95cb20e132bc209a6291659e63aa179bc1
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-23 17:51:39 +0300

    github-api-facade: API_FEATURAS updated

--- src/server/github-api-facade/facade/constants/API_FEATURES.py
+++ src/server/github-api-facade/facade/constants/API_FEATURES.py
@@ -11,7 +11,6 @@ class API_FEATURES(Enum):
     STARGAZERS_COUNT    = 'stargazers_count'   # represents the number of stars that a user has on github
     FORKS_COUNT         = 'forks_count'        # represents the number of forks made by other people from the user github
     COMMITS             = 'commits'            # represents the total number commits that the user made since he started working with github
-    PULL_REQUESTS       = 'pull_requests'      # represents the number of PRs that the user made since he started working with github
     ISSUES              = 'issues'             # represents the number total number of issues that the user took part of 
     ORGS                = 'orgs'               # represents the number of organizations that the user is part of on github
 
