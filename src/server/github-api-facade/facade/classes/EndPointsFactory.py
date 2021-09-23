2a25d179a1c62fb33e1a921723bf2882d9d39b9b
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-23 16:54:24 +0300

    github-api-facade: EndPointsFactory class updated

--- src/server/github-api-facade/facade/classes/EndPointsFactory.py
+++ src/server/github-api-facade/facade/classes/EndPointsFactory.py
@@ -3,10 +3,9 @@ from ..constants.API_FEATURES import API_FEATURES
 class EndPointsFactory:
 
     # Constructor 
-    def __init__(self,base_url,username,feature,repo_name = None):
+    def __init__(self,base_url,username,feature):
         self.username   = username
         self.feature    = feature
-        self.repo_name  = repo_name
         self.base_url   = base_url
 
     # Methods
@@ -28,10 +27,9 @@ class EndPointsFactory:
 
         if self.feature == API_FEATURES.COMMITS.value:
             return f'{self.base_url}/search/commits?q=author:{self.username}'
-            # return f'{self.base_url}/repos/{self.username}/{self.repo_name}/commits'
 
         if self.feature == API_FEATURES.PULL_REQUESTS.value:
-            return f' {self.base_url}/search/issues?q=+type:pr+{self.username}:StackExchange&sort=created&order=asc'
+            return f' {self.base_url}/search/pull?q=+type:pr+{self.username}:StackExchange&sort=created&order=asc'
         
         if self.feature == API_FEATURES.ISSUES.value:
             return f'{self.base_url}/search/issues?q=user:{self.username}'
