53f64c19fd60f93698ce6045b7f1985da2a1508d
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-23 15:51:05 +0300

    github-api-facade: EndPointsFactory class updated

--- src/server/github-api-facade/facade/classes/EndPointsFactory.py
+++ src/server/github-api-facade/facade/classes/EndPointsFactory.py
@@ -3,10 +3,11 @@ from ..constants.API_FEATURES import API_FEATURES
 class EndPointsFactory:
 
     # Constructor 
-    def __init__(self,username,feature,repo_name = None):
+    def __init__(self,base_url,username,feature,repo_name = None):
         self.username   = username
         self.feature    = feature
         self.repo_name  = repo_name
+        self.base_url   = base_url
 
     # Methods
 
@@ -14,27 +15,28 @@ class EndPointsFactory:
     def generate(self):
 
         if self.feature ==  API_FEATURES.FOLLOWERS.value:
-            return  f'https://api.github.com/users/{self.username}' 
+            return f'{self.base_url}/users/{self.username}'
 
         if self.feature == API_FEATURES.PUBLIC_REPOS.value:
-            return f'https://api.github.com/users/{self.username}'
+            return f'{self.base_url}/users/{self.username}'
 
         if self.feature == API_FEATURES.STARGAZERS_COUNT.value:
-            return f'https://api.github.com/users/{self.username}/repos'
+            return f'{self.base_url}/users/{self.username}/repos'
 
         if self.feature == API_FEATURES.FORKS_COUNT.value:
-            return f'https://api.github.com/users/{self.username}/repos'
+            return f'{self.base_url}/users/{self.username}/repos'
 
         if self.feature == API_FEATURES.COMMITS.value:
-            return f'https://api.github.com/users/{self.username}/repos'
+            return f'{self.base_url}/search/commits?q=author:{self.username}'
+            # return f'{self.base_url}/repos/{self.username}/{self.repo_name}/commits'
 
         if self.feature == API_FEATURES.PULL_REQUESTS.value:
-            return f' https://api.github.com/search/issues?q=+type:pr+{self.username}:StackExchange&sort=created&order=asc'
+            return f' {self.base_url}/search/issues?q=+type:pr+{self.username}:StackExchange&sort=created&order=asc'
         
         if self.feature == API_FEATURES.ISSUES.value:
-            return f'https://api.github.com/repos/{self.username}/{self.repo_name}/issues/events'
+            return f'{self.base_url}/repos/{self.username}/{self.repo_name}/issues/events'
 
         if self.feature == API_FEATURES.ORGS.value:
-            return f'https://api.github.com/users/{self.username}/orgs'
+            return f'{self.base_url}/users/{self.username}/orgs'
 
         return None
