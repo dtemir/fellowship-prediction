fd7ea2c7852566cb56f13a0bd83dce8d8f7021ab
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-23 12:24:19 +0300

    github-api-facade: API_FEATURES updated

--- src/server/github-api-facade/facade/API_FEATURES.py
+++ src/server/github-api-facade/facade/API_FEATURES.py
@@ -1,13 +1,17 @@
+from enum import Enum
 ############################################################################################
 #                 THE DIFFERENT FEATURES THAT THIS FACADE API CAN FETCH                    #
 ############################################################################################
 
-FOLLOWERS           = 'followers'          # represents the number of followers that a user has on github
-PUBLIC_REPOS        = 'public_repos'       # represents the number of public repos that a user has on github
-STARGAZERS_COUNT    = 'stargazers_count'   # represents the number of stars that a user has on github
-FORKS_COUNT         = 'forks_count'        # represents the number of forks made by other people from the user github
-COMMITS             = 'commits'            # represents the total number commits that the user made since he started working with github
-PULL_REQUESTS       = 'pull_requests'      # represents the number of PRs that the user made since he started working with github
-ISSUES              = 'issues'             # represents the number total number of issues that the user took part of 
-ORGS                = 'orgs'               # represents the number of organizations that the user is part of on github
+
+class API_FEATURES(Enum):
+    
+    FOLLOWERS           = 'followers'          # represents the number of followers that a user has on github
+    PUBLIC_REPOS        = 'public_repos'       # represents the number of public repos that a user has on github
+    STARGAZERS_COUNT    = 'stargazers_count'   # represents the number of stars that a user has on github
+    FORKS_COUNT         = 'forks_count'        # represents the number of forks made by other people from the user github
+    COMMITS             = 'commits'            # represents the total number commits that the user made since he started working with github
+    PULL_REQUESTS       = 'pull_requests'      # represents the number of PRs that the user made since he started working with github
+    ISSUES              = 'issues'             # represents the number total number of issues that the user took part of 
+    ORGS                = 'orgs'               # represents the number of organizations that the user is part of on github
 
