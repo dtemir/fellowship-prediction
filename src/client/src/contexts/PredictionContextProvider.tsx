f6fb7ed9c0c43eb799850ddb5a410d93058945b7
bennaaym <mr.aymen.bennabi@gmail.com>
2021-09-24 09:27:04 +0300

    client-data: getPrediction method implemented inside PredictionContextProvider

--- src/client/src/contexts/PredictionContextProvider.tsx
+++ src/client/src/contexts/PredictionContextProvider.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, createContext , useContext, ReactNode} from 'react';
+import { useState, createContext , useContext, ReactNode} from 'react';
+import axios from 'axios';
 
 interface IPrediction
 {
-    username:string,
-    setUsername:React.Dispatch<React.SetStateAction<string>>,
-    prediction:{[feature:string]:number}
+    username:string;
+    setUsername:React.Dispatch<React.SetStateAction<string>>;
+    prediction:{[feature:string]:number};
+    getPrediction:() =>Promise<void>,
 }
 
 const PredictionContext = createContext<IPrediction|null>(null);
@@ -15,19 +17,33 @@ export const usePrediction = ():IPrediction =>
     throw Error('NullContextError: PredictionContext is null');
 }
 
-const API_BASE_URL = 'https://fellowship-predict-staging.herokuapp.com/profile';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const PredictionContextProvider:React.FC<ReactNode> = ({children}) => 
 {
 
     const [username,setUsername] = useState<string>('');
-    const [prediction,setPrediction] = useState<IPrediction['prediction']>({})
+    const [prediction,setPrediction] = useState<IPrediction['prediction']>({});
+
+    const getPrediction = async() => 
+    {
+        try
+        {
+            const response = await axios.post(`${API_BASE_URL}/profile`,{username});
+            console.log('response',response);
+        }   
+        catch(error)
+        {
+            console.log('error',error);
+        }
+    }
 
     return(
         <PredictionContext.Provider value={{
             username,
             setUsername,
-            prediction
+            prediction,
+            getPrediction
         }}>
             {children}
         </PredictionContext.Provider>
